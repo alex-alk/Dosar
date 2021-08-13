30fd56741f5b9ec1d843c469b8dd79142491dc69
Alex <alex.leo06@yahoo.ro>
2021-08-13 10:49:15 +0300

    work in progress

--- src/main/java/com/alexandruleonte/Home.java
+++ src/main/java/com/alexandruleonte/Home.java
@@ -5,18 +5,41 @@ import com.alexandruleonte.entities.Platform;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.persistence.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Named
 @RequestScoped
 public class Home {
 
-    @PersistenceContext(unitName = "default")
-    private EntityManager em;
+    private static final String PERSISTENCE_UNIT_NAME = "default";
 
-    public String getHello() {
+    public String getHello() throws ClassNotFoundException, SQLException, IllegalAccessException, InstantiationException {
+//        EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+//        EntityManager em = factory.createEntityManager();
+//
+//        Platform p1 = em.find(Platform.class, 1);
+//        em.close();
+//        return p1.getName();
 
-        Platform p1 = em.find(Platform.class, 1);
+        Connection conn = null;
 
-        return p1.getName();
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Connecting database...");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dosar", "root", "Ewsdew_01");
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
+
+
+        return "OK";
     }
 }
