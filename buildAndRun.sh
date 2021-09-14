9ae72101cdf3a8ee6ee7406f87045dce5fc607da
Alex <alexandruleonte06@yahoo.com>
2021-09-14 21:46:22 +0300

    edited files

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
-mvn clean package && docker build -t com.alexandruleonte/Dosar .
-docker rm -f Dosar || true && docker run -d -p 8080:8080 -p 4848:4848 --name Dosar com.alexandruleonte/Dosar 
+
+#mvn clean package && docker build -t com.alexandruleonte/Dosar .
+#docker rm -f Dosar || true && docker run -d -p 8080:8080 -p 4848:4848 --name Dosar com.alexandruleonte/Dosar
+
+java -jar pm.jar target/ROOT.war --port 8080 --outputUberJar ROOT.jar
