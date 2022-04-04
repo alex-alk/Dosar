31749bd479cf04242f1491170547e6f0b625ddd3
Alex <alexandruleonte06@yahoo.com>
2022-04-04 12:54:53 +0300

    work in progress

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,6 +2,6 @@
 #mvn clean package && docker build -t com.alexandruleonte/Dosar .
 #docker rm -f Dosar || true && docker run -d -p 8080:8080 -p 4848:4848 --name Dosar com.alexandruleonte/Dosar
 
-java -jar ../libs/pm.jar target/ROOT.war --port 80 --sslPort 443 --outputUberJar Dosar.jar
-
-java -jar Dosar.jar --addLibs ../libs/dbc.jar --postbootcommandfile dbc.txt
+mvn clean
+mvn package
+java -jar ../pm.jar target/ROOT.war --addLibs ojdbc11.jar --postbootcommandfile dbc.txt --port 80 --sslPort 443
