d3dd3ce61c8c3ab670de26d53580c09b7f632830
Alex <alexandruleonte06@yahoo.com>
2022-04-05 13:07:25 +0300

    w

--- buildAndRun.sh
+++
@@ -1,7 +0,0 @@
-
-#mvn clean package && docker build -t com.alexandruleonte/Dosar .
-#docker rm -f Dosar || true && docker run -d -p 8080:8080 -p 4848:4848 --name Dosar com.alexandruleonte/Dosar
-
-mvn clean
-mvn package
-java -jar ../pm.jar target/ROOT.war --addLibs ojdbc11.jar --postbootcommandfile dbc.txt --port 80 --sslPort 443
